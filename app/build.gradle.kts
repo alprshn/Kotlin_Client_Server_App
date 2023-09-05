5f3ada1e93be69d17f61df0c8aa48ddbc5541bbc
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-09-05 18:29:21 +0300

    Basic Layout Design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,9 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+    buildFeatures{
+        viewBinding = true
+    }
 
     buildTypes {
         release {
@@ -44,4 +47,11 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //Scable Size Unit
+    implementation("com.intuit.sdp:sdp-android:1.0.6")
+    implementation("com.intuit.ssp:ssp-android:1.0.6")
+
+    //Rounded ImageView
+    implementation("com.makeramen:roundedimageview:2.3.0")
 }
