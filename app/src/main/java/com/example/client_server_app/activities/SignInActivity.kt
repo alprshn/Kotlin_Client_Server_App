2ac5a62edf213aa7953cdd903733ca09f676cfb6
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-09-05 19:41:46 +0300

    Completed Sign In, Sign Up Design

--- app/src/main/java/com/example/client_server_app/activities/SignInActivity.kt
+++ app/src/main/java/com/example/client_server_app/activities/SignInActivity.kt
@@ -1,12 +1,27 @@
 package com.example.client_server_app.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.client_server_app.R
+import com.example.client_server_app.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setListener()
+    }
+
+
+    //Giriş Yapma Sayfasına geçiş fonksiyonu
+    private fun setListener(){
+        binding.textCreateNewAccount.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/client_server_app/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/client_server_app/activities/SignUpActivity.kt
@@ -1,12 +1,26 @@
 package com.example.client_server_app.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.client_server_app.R
+import com.example.client_server_app.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(binding.root)
+        setListener()
+    }
+
+    //Yeni hesap oluşturmaya geçiş fonksiyonu
+   private fun setListener(){
+        binding.textSignIn.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
