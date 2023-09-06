4cef7f51574455430447a88a4626ade2f92a9f0f
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-09-06 17:06:31 +0300

    Edit Firebase

--- app/src/main/java/com/example/client_server_app/activities/SignInActivity.kt
+++ app/src/main/java/com/example/client_server_app/activities/SignInActivity.kt
@@ -1,14 +1,9 @@
 package com.example.client_server_app.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.example.client_server_app.R
+import androidx.appcompat.app.AppCompatActivity
 import com.example.client_server_app.databinding.ActivitySignInBinding
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.ktx.Firebase
-import java.lang.ref.Reference
 
 class SignInActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignInBinding
@@ -26,19 +21,6 @@ class SignInActivity : AppCompatActivity() {
         binding.textCreateNewAccount.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
-        }
-        binding.buttonSignIn.setOnClickListener { v-> addDataToFireStore() }
-    }
-
-    private fun addDataToFireStore() {
-        var database: FirebaseFirestore = FirebaseFirestore.getInstance()
-        var data: HashMap<String, Any> = HashMap()
-        data["first_name"] = "Chirag"
-        data["last_name"] = "Kacchadiva"
-        database.collection("users").add(data).addOnSuccessListener { _ ->
-            Toast.makeText(this, "Data Inserted", Toast.LENGTH_SHORT).show()
-        }.addOnFailureListener{exception ->
-            Toast.makeText(this, exception.message, Toast.LENGTH_SHORT).show()
         }
     }
 }
