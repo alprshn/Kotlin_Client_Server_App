5fb53f3d8fad23f56868a396716d75f2c49f28fe
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-09-09 16:29:39 +0300

    Sign Up Using Firestore Database

--- app/src/main/java/com/example/client_server_app/activities/MainActivity.kt
+++ app/src/main/java/com/example/client_server_app/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.client_server_app
+package com.example.client_server_app.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.client_server_app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/client_server_app/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/client_server_app/activities/SignUpActivity.kt
@@ -3,6 +3,7 @@ package com.example.client_server_app.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.client_server_app.R
 import com.example.client_server_app.databinding.ActivitySignUpBinding
 
@@ -13,14 +14,18 @@ class SignUpActivity : AppCompatActivity() {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        setListener()
+        SetListener()
     }
 
     //Yeni hesap oluşturmaya geçiş fonksiyonu
-   private fun setListener(){
+   private fun SetListener(){
         binding.textSignIn.setOnClickListener {
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun ShowToast(message: String){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
     }
 }
