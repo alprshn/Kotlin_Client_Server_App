3bff4dd1d4fb921fdcc7cf0e4d64c23e0953a1d3
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-09-12 10:21:35 +0300

    Sign Up Using Firestore Database

--- app/src/main/java/com/example/client_server_app/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/client_server_app/activities/SignUpActivity.kt
@@ -1,12 +1,17 @@
 package com.example.client_server_app.activities
 
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
 import android.util.Patterns
+import android.view.View
 import android.widget.Toast
 import com.example.client_server_app.R
 import com.example.client_server_app.databinding.ActivitySignUpBinding
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
 import java.util.regex.Pattern
 
 class SignUpActivity : AppCompatActivity() {
@@ -24,8 +29,8 @@ class SignUpActivity : AppCompatActivity() {
     //Yeni hesap oluşturmaya geçiş fonksiyonu
     private fun SetListener() {
         binding.textSignIn.setOnClickListener { v -> onBackPressed() }
-        binding.buttonSignUp.setOnClickListener { v->
-            if (IsValidSignUpDetails()){
+        binding.buttonSignUp.setOnClickListener { v ->
+            if (IsValidSignUpDetails()) {
                 SignUp()
             }
         }
@@ -34,12 +39,14 @@ class SignUpActivity : AppCompatActivity() {
     private fun ShowToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
+
     private fun SignUp() {
 
     }
+
     private fun IsValidSignUpDetails(): Boolean {
         if (encodedImage == null) {
-            ShowToast("Selet Profile Image")
+            ShowToast("Select Profile Image")
             return false
         } else if (binding.inputName.text.toString().trim().isEmpty()) {
             ShowToast("Enter Name")
@@ -63,6 +70,27 @@ class SignUpActivity : AppCompatActivity() {
             return false
         } else {
             return true
+        }
+    }
+
+    private fun EncodeImage(bitMap: Bitmap): String {
+        var previewWidth: Int = 150
+        var previewHeight: Int = bitMap.height * previewWidth / bitMap.width
+        var previewBitMap: Bitmap =
+            Bitmap.createScaledBitmap(bitMap, previewWidth, previewHeight, false)
+        var byteArrayOutputStream: ByteArrayOutputStream = ByteArrayOutputStream()
+        previewBitMap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream)
+        val bytes: ByteArray = byteArrayOutputStream.toByteArray()
+        return Base64.encodeToString(bytes, Base64.DEFAULT)
+    }
+
+    private fun Loading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.buttonSignUp.visibility = View.INVISIBLE
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.INVISIBLE
+            binding.buttonSignUp.visibility = View.VISIBLE
         }
     }
 }
---
+++ app/src/main/java/com/example/client_server_app/utilities/Constants.kt
@@ -0,0 +1,17 @@
+package com.example.client_server_app.utilities
+
+class Constants {
+    companion object {
+        const val KEY_COLLECTION_USERS = "users"
+        const val KEY_NAME = "name"
+        const val KEY_EMAIL = "email"
+        const val KEY_PASSWORD = "password"
+        const val KEY_PREFERENCE_NAME = "chatAppPreference"
+        const val KEY_IS_SIGNED_IN = "isSignedIn"
+        const val KEY_USER_ID = "userId"
+        const val KEY_IMAGE = "image"
+
+
+    }
+
+}
