49237e09539b9a046b022d5cff9411739af9c578
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-09-17 10:12:46 +0300

    Sign Up Using Firestore Database

---
+++ app/src/main/java/com/example/client_server_app/utilities/PreferenceManager.kt
@@ -0,0 +1,8 @@
+package com.example.client_server_app.utilities
+
+import android.content.SharedPreferences
+
+class PreferenceManager {
+    private lateinit var sharedPreferences: SharedPreferences
+
+}
