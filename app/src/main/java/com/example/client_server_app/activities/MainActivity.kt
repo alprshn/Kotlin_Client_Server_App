a066ef0da20a97c49ca820f7ce7b558f13a846e1
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-09-17 16:14:33 +0300

    Working With FCM Token & Sign Out

--- app/src/main/java/com/example/client_server_app/activities/MainActivity.kt
+++ app/src/main/java/com/example/client_server_app/activities/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.example.client_server_app.activities
 
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.media.session.MediaSession.Token
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Base64
 import android.widget.Toast
-import com.example.client_server_app.R
 import com.example.client_server_app.databinding.ActivityMainBinding
 import com.example.client_server_app.utilities.Constants
 import com.example.client_server_app.utilities.PreferenceManager
 import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.messaging.FirebaseMessaging
-import com.google.firebase.messaging.FirebaseMessagingService
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -27,6 +27,11 @@ class MainActivity : AppCompatActivity() {
         preferenceManager = PreferenceManager(applicationContext)
         LoadUserDetails()
         GetToken()
+        SetListener()
+    }
+
+    private fun SetListener() {
+        binding.imageSignOut.setOnClickListener { v -> SignOut() }
     }
 
     private fun ShowToast(message: String) {
@@ -49,12 +54,28 @@ class MainActivity : AppCompatActivity() {
         FirebaseMessaging.getInstance().token.addOnSuccessListener(this::UpdateToken)
     }
 
-
     private fun LoadUserDetails() {
         binding.textName.text = preferenceManager.getString(Constants.KEY_NAME)
         val bytes: ByteArray =
             Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT)
         var bitmap: Bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
         binding.imageProfile.setImageBitmap(bitmap)
+    }
+
+    private fun SignOut() {
+        ShowToast("Signing Out....")
+        var database: FirebaseFirestore = FirebaseFirestore.getInstance()
+        var documentReference: DocumentReference =
+            database.collection(Constants.KEY_COLLECTION_USERS).document(
+                preferenceManager.getString(Constants.KEY_USER_ID)!!
+            )
+        var updates: HashMap<String, Any> = HashMap()
+        updates.put(Constants.KEY_FCM_TOKEN, FieldValue.delete())
+        documentReference.update(updates)
+            .addOnSuccessListener { unused ->
+                preferenceManager.Clear()
+                startActivity(Intent(applicationContext, SignInActivity::class.java))
+                finish()
+            }.addOnFailureListener { e -> ShowToast("Unable To Sign Out") }
     }
 }
