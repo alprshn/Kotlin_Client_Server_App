831e6190b8a1a17d7d49d4ea7d3a8fd748263e57
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-09-17 13:56:59 +0300

    Sign In Using Firestore Database & Shared Preferences

--- app/src/main/java/com/example/client_server_app/activities/SignInActivity.kt
+++ app/src/main/java/com/example/client_server_app/activities/SignInActivity.kt
@@ -2,6 +2,8 @@ package com.example.client_server_app.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.client_server_app.databinding.ActivitySignInBinding
 
@@ -21,6 +23,36 @@ class SignInActivity : AppCompatActivity() {
         binding.textCreateNewAccount.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
+            binding.buttonSignIn.setOnClickListener { v ->
+                if (IsValidSignInDetails()) {
+                    SignIn()
+                }
+            }
+        }
+
+
+    }
+
+    fun SignIn() {
+
+    }
+
+    private fun ShowToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun IsValidSignInDetails(): Boolean {
+        if (binding.inputEmail.text.toString().trim().isEmpty()) {
+            ShowToast("Enter Email")
+            return false
+        } else if (binding.inputPassword.text.toString().trim().isEmpty()) {
+            ShowToast("Enter Password")
+            return false
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.text.toString()).matches()) {
+            ShowToast("Enter Valid Email")
+            return false
+        } else {
+            return true
         }
     }
 }
