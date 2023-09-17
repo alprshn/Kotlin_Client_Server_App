ba6df15c525914d9b496b86fc6e308b377203a2e
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-09-17 16:05:44 +0300

    Working With FCM Token & Sign Out

--- app/src/main/java/com/example/client_server_app/activities/MainActivity.kt
+++ app/src/main/java/com/example/client_server_app/activities/MainActivity.kt
@@ -1,10 +1,20 @@
 package com.example.client_server_app.activities
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.media.session.MediaSession.Token
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.widget.Toast
 import com.example.client_server_app.R
 import com.example.client_server_app.databinding.ActivityMainBinding
+import com.example.client_server_app.utilities.Constants
 import com.example.client_server_app.utilities.PreferenceManager
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.FirebaseMessagingService
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -14,6 +24,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        preferenceManager = PreferenceManager(applicationContext)
+        LoadUserDetails()
+        GetToken()
+    }
 
+    private fun ShowToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun UpdateToken(token: String) {
+        var database: FirebaseFirestore = FirebaseFirestore.getInstance()
+        var documentReference: DocumentReference =
+            database.collection(Constants.KEY_COLLECTION_USERS)
+                .document(preferenceManager.getString(Constants.KEY_USER_ID)!!)
+        documentReference.update(Constants.KEY_FCM_TOKEN, token)
+            .addOnSuccessListener { unused ->
+                ShowToast("Token Updated Successfully")
+            }
+            .addOnFailureListener { e -> ShowToast("Unable To Updated Token") }
+    }
+
+    private fun GetToken() {
+        FirebaseMessaging.getInstance().token.addOnSuccessListener(this::UpdateToken)
+    }
+
+
+    private fun LoadUserDetails() {
+        binding.textName.text = preferenceManager.getString(Constants.KEY_NAME)
+        val bytes: ByteArray =
+            Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT)
+        var bitmap: Bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+        binding.imageProfile.setImageBitmap(bitmap)
     }
 }
