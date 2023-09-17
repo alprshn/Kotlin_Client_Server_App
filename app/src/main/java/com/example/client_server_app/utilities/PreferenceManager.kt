fe241a3f49ca990a5679a592e9ee29f18ba75dc4
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-09-17 10:24:07 +0300

    Sign Up Using Firestore Database

--- app/src/main/java/com/example/client_server_app/utilities/PreferenceManager.kt
+++ app/src/main/java/com/example/client_server_app/utilities/PreferenceManager.kt
@@ -1,8 +1,39 @@
 package com.example.client_server_app.utilities
 
+import android.content.Context
 import android.content.SharedPreferences
 
 class PreferenceManager {
     private lateinit var sharedPreferences: SharedPreferences
 
+    fun PreferenceManager(context: Context) {
+        sharedPreferences =
+            context.getSharedPreferences(Constants.KEY_PREFERENCE_NAME, Context.MODE_PRIVATE)
+    }
+
+    fun PutBoolean(key: String, value: Boolean) {
+        var editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putBoolean(key, value)
+        editor.apply()
+    }
+
+    fun GetBoolean(key: String): Boolean {
+        return sharedPreferences.getBoolean(key, false)
+    }
+
+    fun PutString(key: String, value: String) {
+        var editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putString(key, value)
+        editor.apply()
+    }
+
+    fun getString(key: String): String? {
+        return sharedPreferences.getString(key, null)
+    }
+
+    fun Clear() {
+        var editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.clear()
+        editor.apply()
+    }
 }
