cf4bf359ebf70e939b25541b395a4d3d815cbab1
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-09-18 12:59:21 +0300

    Display User List From Firestore Database

--- app/src/main/java/com/example/client_server_app/adapters/UsersAdapter.kt
+++ app/src/main/java/com/example/client_server_app/adapters/UsersAdapter.kt
@@ -3,17 +3,42 @@ package com.example.client_server_app.adapters
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.util.Base64
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.client_server_app.databinding.ItemContainerUserBinding
 import com.example.client_server_app.models.User
 import com.example.client_server_app.utilities.Constants
 
-class UsersAdapter {
+class UsersAdapter(private val users: List<User>) :
+    RecyclerView.Adapter<UsersAdapter.UserViewHolder>() {
+
+    private fun GetUserImage(encodedImage: String): Bitmap {
+        val bytes: ByteArray = Base64.decode(encodedImage, Base64.DEFAULT)
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        var itemContainerUserBinding: ItemContainerUserBinding = ItemContainerUserBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return UserViewHolder(itemContainerUserBinding)
+    }
+
+    override fun getItemCount(): Int {
+        return users.size
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        holder.SetUserData(users.get(position))
+    }
+
 
     class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         lateinit var binding: ItemContainerUserBinding
         lateinit var adapter: UsersAdapter
+
         constructor(itemContainerUserBinding: ItemContainerUserBinding) : this(
             itemContainerUserBinding.root
         ) {
@@ -28,8 +53,4 @@ class UsersAdapter {
         }
     }
 
-    private fun GetUserImage(encodedImage: String): Bitmap {
-        val bytes: ByteArray = Base64.decode(encodedImage, Base64.DEFAULT)
-        return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-    }
 }
