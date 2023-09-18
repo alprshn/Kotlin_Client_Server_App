d4fa1fbb13ff4b8bb9617255ae4246c23490ff75
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-09-18 09:52:26 +0300

    Display User List From Firestore Database

---
+++ app/src/main/java/com/example/client_server_app/models/User.kt
@@ -0,0 +1,10 @@
+package com.example.client_server_app.models
+
+import java.io.Serializable
+
+class User : Serializable {
+    lateinit var name: String
+    lateinit var image: String
+    lateinit var email: String
+    lateinit var token: String
+}
