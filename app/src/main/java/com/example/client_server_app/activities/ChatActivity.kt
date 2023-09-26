31660f448473a58be0bd4c78f48d5388060a2d75
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-09-26 14:31:19 +0300

    Chat Screen Layout Design

---
+++ app/src/main/java/com/example/client_server_app/activities/ChatActivity.kt
@@ -0,0 +1,12 @@
+package com.example.client_server_app.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.client_server_app.R
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
