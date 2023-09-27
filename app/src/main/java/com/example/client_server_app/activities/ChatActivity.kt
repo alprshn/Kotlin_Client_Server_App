80c479b11210169e0c5f418543ee88976e6c63f5
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-09-27 18:58:30 +0300

    Chat Data Preparation & ChatAdapter

--- app/src/main/java/com/example/client_server_app/activities/ChatActivity.kt
+++ app/src/main/java/com/example/client_server_app/activities/ChatActivity.kt
@@ -3,10 +3,23 @@ package com.example.client_server_app.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.client_server_app.R
+import com.example.client_server_app.databinding.ActivityChatBinding
+import com.example.client_server_app.models.User
+import com.example.client_server_app.utilities.Constants
 
 class ChatActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityChatBinding
+    private lateinit var receiverUser: User
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat)
+        binding = ActivityChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+
+    private fun LoadReceiverDetails() {
+        receiverUser = (intent.getSerializableExtra(Constants.KEY_USER) as? User)!!
+        binding.textName.text = receiverUser.name
     }
 }
