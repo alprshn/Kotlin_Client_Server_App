eb1638c52611367d99dcb2e4445f9c19edb70432
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-09-28 13:58:35 +0300

    Realtime Chat Implementation

--- app/src/main/java/com/example/client_server_app/utilities/Constants.kt
+++ app/src/main/java/com/example/client_server_app/utilities/Constants.kt
@@ -12,6 +12,11 @@ class Constants {
         const val KEY_IMAGE: String = "image"
         const val KEY_FCM_TOKEN: String = "fcmToken"
         const val KEY_USER: String = "user"
+        const val KEY_COLLECTION_CHAT: String = "chat"
+        const val KEY_SENDER_ID: String = "senderId"
+        const val KEY_RECEIVER_ID: String = "receiverId"
+        const val KEY_MESSAGE: String = "message"
+        const val KEY_TIMESTAMP: String = "timestamp"
 
     }
 
