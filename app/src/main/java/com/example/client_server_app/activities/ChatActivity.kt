fa60c01b7feaecacdb7b255747b06f59b6f091cf
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-09-28 13:54:54 +0300

    Realtime Chat Implementation

--- app/src/main/java/com/example/client_server_app/activities/ChatActivity.kt
+++ app/src/main/java/com/example/client_server_app/activities/ChatActivity.kt
@@ -1,15 +1,26 @@
 package com.example.client_server_app.activities
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
 import com.example.client_server_app.R
+import com.example.client_server_app.adapters.ChatAdapter
 import com.example.client_server_app.databinding.ActivityChatBinding
+import com.example.client_server_app.models.ChatMessage
 import com.example.client_server_app.models.User
 import com.example.client_server_app.utilities.Constants
+import com.example.client_server_app.utilities.PreferenceManager
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ChatActivity : AppCompatActivity() {
     private lateinit var binding: ActivityChatBinding
     private lateinit var receiverUser: User
+    private lateinit var chatMessages: List<ChatMessage>
+    private lateinit var chatAdapter: ChatAdapter
+    private lateinit var preferenceManager: PreferenceManager
+    private lateinit var database: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,9 +28,25 @@ class ChatActivity : AppCompatActivity() {
         setContentView(binding.root)
         SetListeners()
         LoadReceiverDetails()
-
+        init()
     }
 
+    private fun init() {
+        preferenceManager = PreferenceManager(applicationContext)
+        chatMessages = ArrayList()
+        chatAdapter = ChatAdapter(
+            GetBitmapFromEncodedString(receiverUser.image),
+            chatMessages,
+            preferenceManager.getString(Constants.KEY_USER_ID).toString()
+        )
+        binding.chatRecyclerView.adapter = chatAdapter
+        database = FirebaseFirestore.getInstance()
+    }
+
+    private fun GetBitmapFromEncodedString(encodedImage: String): Bitmap {
+        val bytes: ByteArray = Base64.decode(encodedImage, Base64.DEFAULT)
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+    }
 
     private fun LoadReceiverDetails() {
         receiverUser = intent.getSerializableExtra(Constants.KEY_USER) as User
