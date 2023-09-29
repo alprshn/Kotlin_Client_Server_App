14251614114cef57296b01d384883c1d67848ba6
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-09-29 16:05:07 +0300

    Recent Conversations

--- app/src/main/java/com/example/client_server_app/activities/MainActivity.kt
+++ app/src/main/java/com/example/client_server_app/activities/MainActivity.kt
@@ -6,22 +6,26 @@ import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Base64
+import android.view.View
 import android.widget.Toast
 import com.example.client_server_app.adapters.RecentConversationsAdapter
 import com.example.client_server_app.databinding.ActivityMainBinding
 import com.example.client_server_app.models.ChatMessage
 import com.example.client_server_app.utilities.Constants
 import com.example.client_server_app.utilities.PreferenceManager
+import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.EventListener
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.messaging.FirebaseMessaging
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var preferenceManager: PreferenceManager
-    private lateinit var conversations: List<ChatMessage>
+    private lateinit var conversations: ArrayList<ChatMessage>
     private lateinit var conversationsAdapter: RecentConversationsAdapter
     private lateinit var database: FirebaseFirestore
 
@@ -35,7 +39,7 @@ class MainActivity : AppCompatActivity() {
         SetListener()
     }
 
-    private fun init(){
+    private fun init() {
         conversations = ArrayList()
         conversationsAdapter = RecentConversationsAdapter(conversations)
         binding.conversationsRecyclerView.adapter = conversationsAdapter
@@ -46,6 +50,74 @@ class MainActivity : AppCompatActivity() {
         binding.imageSignOut.setOnClickListener { v -> SignOut() }
         binding.fabNewChat.setOnClickListener { v ->
             startActivity(Intent(applicationContext, UsersActivity::class.java))
+        }
+    }
+
+    val eventListener = EventListener<QuerySnapshot> { value, error ->
+        if (error !== null) {
+            return@EventListener
+        }
+
+        if (value != null) {
+            var documentChange: DocumentChange
+            for (documentChange in value.getDocumentChanges()) {
+                if (documentChange.type == DocumentChange.Type.ADDED) {
+                    var senderId: String =
+                        documentChange.document.getString(Constants.KEY_SENDER_ID).toString()
+                    var receiverId: String =
+                        documentChange.document.getString(Constants.KEY_RECEIVER_ID).toString()
+                    var chatMessage: ChatMessage = ChatMessage()
+                    chatMessage.senderId = senderId
+                    chatMessage.receiverId = receiverId
+                    if (preferenceManager.getString(Constants.KEY_USER_ID).equals(senderId)) {
+                        chatMessage.conversionImage =
+                            documentChange.document.getString(Constants.KEY_RECEIVER_IMAGE)
+                                .toString()
+                        chatMessage.conversionName =
+                            documentChange.document.getString(Constants.KEY_RECEIVER_NAME)
+                                .toString()
+                        chatMessage.conversionId =
+                            documentChange.document.getString(Constants.KEY_RECEIVER_ID).toString()
+                    } else {
+                        chatMessage.conversionImage =
+                            documentChange.document.getString(Constants.KEY_SENDER_IMAGE)
+                                .toString()
+                        chatMessage.conversionName =
+                            documentChange.document.getString(Constants.KEY_SENDER_NAME)
+                                .toString()
+                        chatMessage.conversionId =
+                            documentChange.document.getString(Constants.KEY_SENDER_ID).toString()
+                    }
+                    chatMessage.message =
+                        documentChange.document.getString(Constants.KEY_LAST_MESSAGE).toString()
+                    chatMessage.dateObject =
+                        documentChange.document.getDate(Constants.KEY_TIMESTAMP)!!
+                    conversations.add(chatMessage)
+                } else if (documentChange.type == DocumentChange.Type.MODIFIED) {
+                    for (i in 0 until conversations.size) {
+                        var senderId: String =
+                            documentChange.document.getString(Constants.KEY_SENDER_ID).toString()
+                        var receiverId: String =
+                            documentChange.document.getString(Constants.KEY_RECEIVER_ID).toString()
+                        if (conversations.get(i).senderId.equals(senderId) && conversations.get(i).receiverId.equals(
+                                receiverId
+                            )
+                        ) {
+                            conversations.get(i).message =
+                                documentChange.document.getString(Constants.KEY_LAST_MESSAGE)
+                                    .toString()
+                            conversations.get(i).dateObject =
+                                documentChange.document.getDate(Constants.KEY_TIMESTAMP)!!
+                            break
+                        }
+                    }
+                }
+            }
+            conversations.sortWith(compareBy { it.dateObject })
+            conversationsAdapter.notifyDataSetChanged()
+            binding.conversationsRecyclerView.smoothScrollToPosition(0)
+            binding.conversationsRecyclerView.visibility = View.VISIBLE
+            binding.progressBar.visibility = View.GONE
         }
     }
 
